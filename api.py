a185033a56e3ce8b49b60a29f10e0b1bd354f163
venkat <venkatesh18596@gmail.com>
2025-01-10 19:42:59 +0530

    Added option to retrive any existing file

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import traceback
 import os
 from fastapi import FastAPI, File, UploadFile, Header
 from fastapi.staticfiles import StaticFiles
@@ -27,9 +28,27 @@ def upload(file: UploadFile = File(...)):
         with open(os.path.join("output", res), 'r') as f:
             return {"xml": f.read()}
     except Exception as e:
-        print(e)
+        print(traceback.format_exc())
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
 
     return f"{origin}/html/{res}"
+
+@app.get("/get/{fileId}")
+def read_root(fileId: str):
+    fPath = os.path.join("output", f'{fileId}.xml')
+    if os.path.exists(fPath):
+        with open(fPath, 'r') as f:
+            return {"xml": f.read()}
+    else:
+        return {"message": "File not exists"}
+
+@app.get("/get")
+def read_root():
+    all_files = os.listdir("output")
+    out = []
+    for f in all_files:
+        if f.endswith(".xml"):
+            out.append(f.replace(".xml", ""))
+    return {"files": out}
